8e4c37d0b955369aef0c4e6e076e47c1de979dee
onifade boluwatife <48095055+onifs10@users.noreply.github.com>
2020-10-19 15:05:25 +0100

    first commit

---
+++ app/Models/PollingUnits.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PollingUnits extends Model
+{
+    use HasFactory;
+
+    protected $table = 'polling_unit';
+    
+}
